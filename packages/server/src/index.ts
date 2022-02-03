57fec3e8db4538f596a2dfe3706e373df422f31b
viktoria <vikaberezuk12345@gmail.com>
2022-02-03 15:05:34 +0200

    test node

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,18 +1,9 @@
-import express from 'express';
-import path from 'path';
-// import dotenv from 'dotenv';
+import { App } from './App';
 
-// dotenv.config();
+const { express } = new App();
 
-const server = express();
 const PORT = process.env.PORT || 3000;
 
-server.use(express.static('./../web/dist'));
-server.get('/', (req, response) => {
-  response.sendFile(path.resolve(path.resolve(), 'static', './../../web/dist/index.html'));
-  response.status(200);
-});
-
-server.listen(PORT, () => {
+express.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
